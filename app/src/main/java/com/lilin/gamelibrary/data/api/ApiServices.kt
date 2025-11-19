28d0ad6d4e07a7c487491d28412ad88fb70842bb
lilin <st.yuto0410@gmail.com>
2025-11-19 23:16:02 +0900

    apiservice修正

--- app/src/main/java/com/lilin/gamelibrary/data/api/ApiServices.kt
+++ app/src/main/java/com/lilin/gamelibrary/data/api/ApiServices.kt
@@ -2,15 +2,15 @@ package com.lilin.gamelibrary.data.api
 
 import com.lilin.gamelibrary.data.dto.GamesResponse
 import retrofit2.Response
-import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
-import javax.inject.Inject
 
 interface ApiServices {
     @GET("games")
     suspend fun getGames(
         @Query("page") page: Int,
         @Query("page_size") pageSize: Int,
+        @Query("dates") dates: String,
+        @Query("ordering") ordering: String,
     ): Response<GamesResponse>
 }
