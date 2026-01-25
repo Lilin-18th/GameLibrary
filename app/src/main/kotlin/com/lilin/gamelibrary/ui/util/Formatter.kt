43b2f0524309ff24e805c09c85ed8781ec806ba2
lilin <st.yuto0410@gmail.com>
2026-01-25 22:23:57 +0900

    formatter作成

---
+++ app/src/main/kotlin/com/lilin/gamelibrary/ui/util/Formatter.kt
@@ -0,0 +1,19 @@
+package com.lilin.gamelibrary.ui.util
+
+fun Double.formatAvgRating(): String {
+    return if (this > 0) {
+        "%.1f".format(this)
+    } else {
+        "-"
+    }
+}
+
+fun Double.formatAvgRatingWithStar(): String {
+    return if (this > 0) {
+        "★ ${"%.1f".format(this)}"
+    } else {
+        "-"
+    }
+}
+
+
